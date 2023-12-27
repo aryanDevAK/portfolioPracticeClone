87b99e521ea01c84e38df5c2cf0a461e35ed44b5
AK-Developers <aryankhatri9811@gmail.com>
2023-12-27 18:02:38 +0530

    react

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import { NavBar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
+import { Footer } from './components/Footer';
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
       <Skills />
-      <Projects/>
+      <Projects />
+      <Footer/>
     </div>
   );
 }
